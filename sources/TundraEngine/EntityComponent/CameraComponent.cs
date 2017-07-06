606efe531c461c3c48d9a2720be689ec50ad1da5
Jeremie St-Amand <jeremie.stamand@gmail.com>
2017-07-06 15:29:42 -0400

    Camera defaults

--- sources/TundraEngine/EntityComponent/CameraComponent.cs
+++ sources/TundraEngine/EntityComponent/CameraComponent.cs
@@ -14,5 +14,11 @@
         public float FarPlane;
 
         public static readonly StringId32 Type = "Camera";
+
+        public const float DefaultAspectRatio = 16f / 9f;
+        public const float DefaultOrthographicSize = 10f;
+        public const float DefaultVerticalFieldOfView = 45f;
+        public const float DefaultNearClipPlane = 0.1f;
+        public const float DefaultFarClipPlace = 100f;
     }
 }
