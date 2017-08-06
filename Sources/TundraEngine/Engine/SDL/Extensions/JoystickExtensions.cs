d44dd17c5f5917417ef0fade5ddf6e8d69113cc9
Jeremie St-Amand <jeremie.stamand@gmail.com>
2017-08-06 01:49:50 -0400

    SDL joystick

---
+++ Sources/TundraEngine/Engine/SDL/Extensions/JoystickExtensions.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Runtime.CompilerServices;
+
+namespace Engine.SDL
+{
+    public static class JoystickExtensions
+    {
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        public static Joystick CheckErrorAndReturn(this Joystick joystick, string message)
+        {
+            Assert.IsTrue(joystick.NativeHandle != IntPtr.Zero, "[SDL] " + message + ": " + SDL.GetError());
+            return joystick;
+        }
+    }
+}
--- Sources/TundraEngine/Engine/SDL/Joystick.cs
+++ Sources/TundraEngine/Engine/SDL/Joystick.cs
@@ -1,5 +1,7 @@
 ﻿using System;
 
+using static Engine.SDL.SDL;
+
 namespace Engine.SDL
 {
     public enum JoystickPowerLevel
@@ -34,6 +36,56 @@ namespace Engine.SDL
 
     public struct Joystick
     {
+        //
+        // Members
+        //
         internal IntPtr NativeHandle;
+
+        //
+        // Properties
+        //
+        public unsafe string Name => GetString(Native.SDL_JoystickName(this));
+
+        public JoystickID InstanceID => Native.SDL_JoystickInstanceID(this);
+
+        public int NumAxes => Native.SDL_JoystickNumAxes(this);
+
+        public int NumBalls => Native.SDL_JoystickNumBalls(this);
+
+        public int NumHats => Native.SDL_JoystickNumHats(this);
+
+        public int NumButtons => Native.SDL_JoystickNumButtons(this);
+
+        public JoystickPowerLevel CurrentPowerLevel => Native.SDL_JoystickCurrentPowerLevel(this);
+
+        //
+        // Constructors
+        //
+        public Joystick(int deviceIndex)
+        {
+            this = Native.SDL_JoystickOpen(deviceIndex).CheckErrorAndReturn("Could not open joystick " + deviceIndex);
+        }
+
+        public Joystick(JoystickID joystickID)
+        {
+            this = Native.SDL_JoystickFromInstanceID(joystickID);
+        }
+
+        //
+        // Methods
+        //
+        public Guid GetGuid() => Native.SDL_JoystickGetGUID(this);
+
+        public bool GetAttached() => Native.SDL_JoystickGetAttached(this);
+
+        public short GetAxisState(JoystickAxis axis) => Native.SDL_JoystickGetAxis(this, axis);
+
+        public JoystickHat GetHatState(int hat) => Native.SDL_JoystickGetHat(this, hat);
+
+        public unsafe int GetBallState(int ball, int* dx, int* dy) => Native.SDL_JoystickGetBall(this, ball, dx, dy).CheckErrorAndReturn("Could not get ball state");
+
+        public byte GetButtonState(int button) => Native.SDL_JoystickGetButton(this, button);
+
+        public void Close() => Native.SDL_JoystickClose(this);
     }
 }
